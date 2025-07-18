9f99ba81676bbb73f0e831420484ece8aba153f4
hryzion <13651325353@sina.cn>
2025-07-18 16:45:07 +0800

    preprocess

---
+++ config.py
@@ -0,0 +1,10 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    
+    args = parser.parse_args()
+    return args
+
+
+DATA_DIR = rf"D:\zhx_workspace\3DScenePlatformDev\dataset\Levels2021"
--- utils.py
+++ utils.py
@@ -1,7 +1,28 @@
 import json
 
 
-def transform_scene_json_as_matrix(scene_json):
+def transform_room_to_matrix(room):
     pass
+
+
+def load_scene_json(scene_json_path):
+    with open(scene_json_path, 'r') as file:
+        scene_json = json.load(file)
+    return scene_json
+
+def divide_scene_json_to_rooms(scene_json):
+    rooms = []
+    for room in scene_json['rooms']:
+        obj_list = room['objList']
+        count = 0
+        for obj in obj_list:
+            if 'coarseSemantic' not in obj or obj["coarseSemantic"] == 'Window' or obj['coarseSemantic'] == 'Door':
+                continue
+            count += 1
+        if count > 0:
+            rooms.append(room)
+    return rooms
+
+
 
 
